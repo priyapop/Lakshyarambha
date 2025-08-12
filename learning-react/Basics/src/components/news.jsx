49526b178cbbc4b4752ffb9d365599d62fdd130e
priyapop <dangolpriya21@gmail.com>
2025-08-12 20:13:26 +0545

    news app

--- learning-react/Basics/src/components/news.jsx
+++ learning-react/Basics/src/components/news.jsx
@@ -1,33 +1,57 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const News = () => {
   const [news, setNews] = useState([]);
-  const [query, setQuery] = useState('nepal');
-  const url =
-    `https://newsapi.org/v2/everything?q=${query}&from=2025-07-11&sortBy=publishedAt&apiKey=72d5890d607444bf9e24ec39703044ae`
-  const fetchNews = async () => {
+  const [query, setQuery] = useState("apple");
+
+  const fetchNews = async (query) => {
+    const url = `https://newsapi.org/v2/everything?q=${query}&from=2025-07-12&sortBy=publishedAt&apiKey=a19ccd4d6d564169b478dde51113e2f5`;
+
     const response = await axios.get(url);
     console.log(response, "response");
     setNews(response?.data?.articles);
   };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchNews(query);
+  };
+
   useEffect(() => {
-    fetchNews();
+    fetchNews(query);
   }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">News</h1>
-      {/* <input type="text" value={query} onChange={()=>setQuery(e.target.value)} placeholder="enter value"}></input> */}
-      {news.map((item, index) => {
-        <div key={index}>
-          <div></div>
-          <div className="flex felx-col gap-2">
-            <h2>{item.title}</h2>
-            <h2>{item.description}</h2>
-            <h2>{item.author}</h2>
-            <h2>{item.publishedAt}</h2>
+    <div className="p-16">
+      <h1 className="text-3xl text-white-500 text mb-6">NEWS</h1>
+      <form className="py-4" onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="enter any query "
+          className=" border-black  border-2"
+        />
+        <button type="submit" className="ml-2 px-4 py-2 bg-blue-500 rounded-xs">
+          Search
+        </button>
+      </form>
+
+      <div className="grid grid-cols-3 gap-8">
+        {news.map((item, index) => (
+          <div key={index}>
+            <div className="flex flex-col gap-2 border-2  p-4">
+              <img src={item?.urlToImage} className="h-[400px] w-full" />
+
+              <h2 className="font-bold">{item?.title}</h2>
+              <h4 className="font-normal">{item?.description}</h4>
+              <h4>{item?.author}</h4>
+              <h4>{item?.publishedAt}</h4>
+            </div>
           </div>
-        </div>;
-      })}
+        ))}
+      </div>
     </div>
   );
 };
