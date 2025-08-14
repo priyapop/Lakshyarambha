64eafb5bfd76fc359d58149974d541c3c27c56f9
priyapop <dangolpriya21@gmail.com>
2025-08-14 21:40:15 +0545

    id bar with onclick function to change post

--- learning-react/Basics/src/components/random.jsx
+++ learning-react/Basics/src/components/random.jsx
@@ -3,12 +3,19 @@ import { useState, useEffect } from "react";
 
 export const Random = () => {
   const [post, setPost] = useState([]);
+  const [allPost, setAllPost] = useState([]);
   const [uid, setuid] = useState(1);
   const fetchPost = async () => {
     const url = "https://jsonplaceholder.typicode.com/posts";
     const response = await axios.get(url);
     console.log(response, "response");
     setPost(response.data);
+    setAllPost(response.data)
+  };
+
+  const handleidChange = (id) => {
+    setPost(allPost.filter((nid) => nid.userId === id));
+    setuid(id);
   };
 
   useEffect(() => {
@@ -19,32 +26,30 @@ export const Random = () => {
     <>
       <h1 className="text-2xl pb-2">POSTS</h1>
       <div>
-        {/* {post.map((item) => (
-          <div className="grid grid-flow-rows gap-2 " key={item.id}>
-            
-          </div>
-        ))} */}
         <div className="flex flex-row gap-8 pb-6">
-          {post
+          {allPost
             .filter(
               (item, index, self) =>
                 index === self.findIndex((t) => t.userId === item.userId)
             )
             .map((item) => (
-              <div className="text-black w-8 h-8  rounded-full bg-blue-100  " key={item.userId}>
-                <h1 className="self-center">
+              <div
+                className="text-black w-8 h-8  rounded-full bg-blue-100  "
+                key={item.userId}
+              >
+                <h1
+                  className="self-center"
+                  onClick={() => handleidChange(item.userId)}
+                >
                   {item.userId}
                 </h1>
               </div>
             ))}
         </div>
-
-        {post.map((item, uid) => (
+        {post.map((item) => (
           <div className="grid grid-flow-rows gap-2 " key={item.id}>
             <div className="border-y border-white-100 pt-4  pb-4">
-              {/* <h1 className=" text-blue-500  ">{item.userId === 1 && item.title}</h1>
-              <p>{item.userId === 1 && item.body}</p> */}
-              {item.userId === 1 && (
+              {item.userId === uid && (
                 <>
                   <h1 className="w-5 h-5 rounded-full bg-blue-100">
                     {item.userId}
