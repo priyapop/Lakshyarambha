c5533f3a0220d18090e68642d4bd418e8360c6b0
priyapop <dangolpriya21@gmail.com>
2025-08-19 22:15:45 +0545

    update delete blog

--- learning-node/express-practice/controller/blogController.js
+++ learning-node/express-practice/controller/blogController.js
@@ -10,3 +10,65 @@ export const createBlog = async (req,res) =>{
         })
     }
 }
+export const updateBlog = async (req, res) => {
+  try {
+    const { id } = req.params; // blog id from URL
+    const blog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true, // return the updated doc
+      runValidators: true, // validate before saving
+    });
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json(blog);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+export const deleteBlog = async (req, res) => {
+  try {
+    const { id } = req.params; // blog id from URL
+    const blog = await Blog.findByIdAndDelete(id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+export const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json(blogs);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json(blog);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
--- learning-node/express-practice/routes/blogRoutes.js
+++ learning-node/express-practice/routes/blogRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express'
-import {createBlog} from '../controller/blogController.js'
+import {createBlog,updateBlog,deleteBlog,getBlogs,getBlogById} from '../controller/blogController.js'
 
  const router = express.Router()
  router.post("/createblog",createBlog)
+ router.put("/updateblog/:id", updateBlog)
+router.delete("/deleteblog/:id", deleteBlog);
+router.get("/blogs", getBlogs);       // all blogs
+router.get("/blogs/:id", getBlogById); // single blog
  export default router
