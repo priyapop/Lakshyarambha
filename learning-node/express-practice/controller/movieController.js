9ee5a30ae8b4b29f079ec516a4127b329dcfc28a
priyapop <dangolpriya21@gmail.com>
2025-08-21 19:21:58 +0545

    get update delete

--- learning-node/express-practice/controller/movieController.js
+++ learning-node/express-practice/controller/movieController.js
@@ -3,7 +3,83 @@ export const createMovie = async (req, res) => {
   try {
     const movie = await Movie.create(req.body);
     res.status(201).json(movie);
-  } catch(err) {
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    })
+  }
+}
+export const getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.status(200).json(movies);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    })
+  }
+}
+export const getMoviesById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findById(id);
+
+    if (!movie) {
+      return res.status(404).json({ message: "movie not found" });
+    }
+
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    })
+  }
+}
+export const getMoviesByGenre = async (req, res) => {
+  try {
+    const { gg} = req.params;
+    const movie = await Movie.find({genre:gg});
+
+    if (!movie) {
+      return res.status(404).json({ message: "movie not found" });
+    }
+
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+}
+export const updateMovies = async (req,res)=>{
+  try{
+const {id}= req.params
+const movie = await Movie.findByIdAndUpdate(id,req.body, {
+      new: true, 
+      runValidators: true, 
+    })
+        if (!movie) {
+      return res.status(404).json({ message: "movie not found" });
+    }
+    res.status(200).json(movie);
+  }
+  catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+}
+export const deleteMovie = async (req, res) => {
+  try {
+    const { id } = req.params
+    const movie = await Movie.findByIdAndDelete(id);
+
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
+    res.status(200).json({ message: "Movie deleted successfully" });
+  } catch (err) {
     res.status(500).json({
       error: err.message,
     });
--- learning-node/express-practice/routes/movieRoutes.js
+++ learning-node/express-practice/routes/movieRoutes.js
@@ -1,5 +1,10 @@
 import express from 'express'
-import { createMovie } from '../controller/movieController.js'
+import { createMovie,getMovies,getMoviesById,getMoviesByGenre,updateMovies,deleteMovie} from '../controller/movieController.js'
 const router = express.Router()
 router.post("/createmovie",createMovie)
+router.get("/getmovie",getMovies)
+router.get("/getmovie/:id",getMoviesById)
+router.get("/getmovie/genre/:gg",getMoviesByGenre)
+router.put("/updatemovies/:id",updateMovies)
+router.delete("/deletemovies/:id",deleteMovie)
  export default router
